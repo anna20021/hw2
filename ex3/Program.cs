483a2462387b18c43d564b0eda37eb18383b481d
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-20 08:53:38 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("number");
+int number= int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("x= negative y= positive");
+}
+if (number == 2)
+{
+    Console.WriteLine("x= positive y= positive");
+}
+if (number == 3)
+{
+    Console.WriteLine("x= positive y= negative");
+}
+if (number == 4)
+{
+    Console.WriteLine("x= negative y= negative");
+}
