b2f608b63bab7b6353dcfed600412b53efbdbef9
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-20 08:46:14 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("numberA");
+int numberA= int.Parse(Console.ReadLine());
+Console.WriteLine("numberB");
+int numberB= int.Parse(Console.ReadLine());
+int a = 0;
+a = numberA*numberA;
+if (a ==numberB)
+{
+    Console.WriteLine("yes");
+}
+else if ( a!= numberB)
+{
+    Console.WriteLine("no");
+}
