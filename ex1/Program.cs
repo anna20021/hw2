c7e3a93b13d0405e5bd01f44dd3436d7b7430b12
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-20 08:32:22 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine ("number");
+int number = int.Parse(Console.ReadLine());
+string [] weekday = new string[7] { "mon", "tue", "wen", "thu", "fri", "sat", "sun"};;
+int i= weekday.Length;
+if (number < i && number < 6)
+{
+    Console.WriteLine("not day off");
+}
+else if ( number < i && number <= 7)
+{
+    Console.WriteLine("day off");
+}
+
